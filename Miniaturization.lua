cfe36701fe94ba4b46d301ca77159e50a96e7f36
defy27 <114071866+defy27@users.noreply.github.com>
2022-09-21 13:39:53 +0200

    Update Miniaturization.lua

--- Miniaturization.lua
+++ Miniaturization.lua
@@ -12,30 +12,57 @@ function placer()
 end
 
 function build()
+	robot.select(1)
 	placer()
 	robot.forward()
 	placer()
 	robot.forward()
 	placer()
 	robot.turnRight()
+	
 	robot.forward()
 	placer()
 	robot.forward()
 	placer()
 	robot.turnRight()
+	
 	robot.forward()
 	placer()
 	robot.turnRight()
+	
 	robot.forward()
-	placer()
+	robot.down()
+	robot.down()
+	robot.placeDown()
+	robot.up()
+	robot.select(2)
+	robot.placeDown()
+	robot.up()
+	robot.select(1)
+	robot.placeDown()
+	robot.turnLeft()
+	
 	robot.forward()
 	placer()
 	robot.turnLeft()
-	robot.forward()
-	placer()
-	robot.turnLeft()
+	
 	robot.forward()
 	placer()
 	robot.forward()
 	placer()
 end
+
+while robot.count(1)>25 and robot.count(2)>0 and robot.count(3)>0 do
+	robot.turnRight()
+	robot.select(1)
+	robot.suck(26)
+	if robot.count(1)<26 then
+		robot.suck(26-robot.count(1))
+	end
+	robot.turnRight()
+	robot.select(2)
+	robot.suck(1)
+	robot.turnRight()
+	robot.select(3)
+	robot.suck(1)
+	
