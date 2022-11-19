a0c02f30f594e89d67890419e2a3198daaf2a2de
defy27 <114071866+defy27@users.noreply.github.com>
2022-11-19 20:28:20 +0100

    Update sewage.lua

--- sewage.lua
+++ sewage.lua
@@ -1,9 +1,52 @@
 local robot = require("robot")
 
-function poop()
-	
+function fluid()
+	while not robot.detectUp() do
+		robot.up()
+	end
+	robot.turnRight()
+	robot.turnRight()
+	while not robot.detect() do
+		robot.forward()
+	end
+	robot.turnRight()
+	while not robot.detect() do
+		robot.forward()
+	end
+	robot.down()
+	robot.down()
+	robot.down()
+	robot.down()
+	robot.down()
+	robot.select(1)
+	while robot.count(1)>0 and robot.tankSpace()>1000 do
+		robot.dropDown(1)
+		robot.drainDown()
+		os.sleep(1)
+	end
+	robot.turnRight()
+	robot.suck(32)
+	while not robot.detectUp() do
+		robot.up()
+	end
+	robot.turnRight()
+	while not robot.detect() do
+		robot.forward()
+	end
+	robot.turnLeft()
+	robot.forward()
+	robot.forward()
+	robot.forward()
+	robot.forward()
+	while not robotdetectDown() do
+		robot.down()
+	end
+	while robot.tankLevel()>0 do
+		robot.fill()
+	end
 end
 
-while true do
-	
+
+while robot.count(1)>0 and robot.tankLevel()<1000 do
+	fluid()
 end
